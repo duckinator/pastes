4f8f80ee1e504222131d54052cf7a1d87ad8ca4e
Ellen Marie Dash <me@duckie.co>
2018-01-11 02:59:37 -0500

    Add paste: theme.js

---
+++ theme.js
@@ -0,0 +1,21 @@
+exports.colors = {
+  dark: {
+    accent0: "#000000",
+    accent1: "#EE0E00",
+    accent2: "#00AA00",
+    accent3: "#AA5500",
+    accent4: "#3C3C75",
+    accent5: "#AA00AA",
+    accent6: "#00AAAA",
+    accent7: "#CCCCCC",
+
+    shade0: "#555555",
+    shade1: "#FF5555",
+    shade2: "#55FF55",
+    shade3: "#FFFF55",
+    shade4: "#A2A2FF",
+    shade5: "#FF55FF",
+    shade6: "#55FFFF",
+    shade7: "#FFFFFF",
+  },
+};
